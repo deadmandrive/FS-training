ac1c2779de3e488c6ded2b0faa473dd6e0c6a7a6
Peeradis S <die9994@hotmail.com>
2023-03-24 22:20:56 +0700

    - init

---
+++ dotnetcore5kafka/Controllers/KafkaProducerController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace dotnetcore5kafka.Controllers
+{
+    [Route("api/kafka")]
+    [ApiController]
+    public class KafkaProducerController : ControllerBase
+    {
+        private ProducerConfig producerConfig;
+        private SchemaRegistryConfig schemaregistryConfig;
+
+
+        private readonly ProducerConfig config = new ProducerConfig
+        { BootstrapServers = "localhost:9092" };
+        private readonly string topic = "simpletalk_topic";
+
+
+        public KafkaProducerController(ProducerConfig producerConfig,SchemaRegistryConfig schemaregistryConfig)
+        {
+            this.producerConfig = producerConfig;
+            this.schemaregistryConfig = schemaregistryConfig;
+            this.producerConfig.SaslMechanism = SaslMechanism.ScramSha512;
+
+
+        }
+
+        [HttpPost("produce")]
+        public async Task<ActionResult> ProduceMessage(string topic, [FromBody] ApplicationLog message)
+        {
+            string serailizedTestData = JsonConvert.SerializeObject(message);
+            // Note: Specifying json serializer configuration is optional.
+            var jsonSerializerConfig = new JsonSerializerConfig
+            {
+                BufferBytes = 100
+            };
+
+            using (var schemaRegistry = new CachedSchemaRegistryClient(schemaregistryConfig))
+            {
+                //var schema = await schemaRegistry.GetLatestSchemaAsync(SubjectNameStrategy.Topic.ConstructValueSubjectName(topic));
+                
+                using (var producer = new ProducerBuilder<string, ApplicationLog>(producerConfig)
+                    .SetValueSerializer(new JsonSerializer<ApplicationLog>(schemaRegistry, jsonSerializerConfig)).Build())
+                {
+                   
+                    await producer.ProduceAsync(topic, new Message<string, ApplicationLog> { Value = message });
+                    producer.Flush(TimeSpan.FromSeconds(10));
+
+
+                }
+            }
+
+            //using (var schemaRegistry = new CachedSchemaRegistryClient(schemaregistryConfig))
+            //{
+            //    // Note: a subject name strategy was not configured, so the default "Topic" was used.
+            //    var schema = await schemaRegistry.GetLatestSchemaAsync(SubjectNameStrategy.Topic.ConstructValueSubjectName(topic));
+               
+            //    Console.WriteLine("\nThe JSON schema corresponding to the written data:");
+            //    Console.WriteLine(schema.SchemaString);
+            //}
+            return Ok(true);
+        }
+
+       
+
+    }
+
+    public class TestData
+    {
+        [JsonRequired]
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonRequired]
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonRequired]
+        [JsonProperty("address")]
+        public string address { get; set; }
+        [JsonRequired]
+        [JsonProperty("tel")]
+        public string tel { get; set; }
+        [JsonProperty("isadmin")]
+        public bool isAdmin { get; set; }
+    }
+
+    public class ApplicationLog
+    {
+        public DateTime TimeStamp { get; set; }
+        public string? LogLevel { get; set; }
+        public string? LogSystem { get; set; }
+        public string? User { get; set; }
+        public string? Module { get; set; }
+        public string? SubModule { get; set; }
+        public string? HttpStatusCode { get; set; }
+        public string? HttpMethod { get; set; }
+        public string? LogMessage { get; set; }
+
+    }
+}
+
---
+++ dotnetcore5kafka/Startup.cs
@@ -0,0 +1,67 @@
+using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace dotnetcore5kafka
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var producerConfig = new ProducerConfig();
+            Configuration.Bind("Producer", producerConfig);
+            var schemaregistryConfig = new SchemaRegistryConfig();
+            Configuration.Bind("SchemaRegistry", schemaregistryConfig);
+            
+            services.AddSingleton<ProducerConfig>(producerConfig);
+            services.AddSingleton<SchemaRegistryConfig>(schemaregistryConfig);
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "dotnetcore5kafka", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "dotnetcore5kafka v1"));
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
